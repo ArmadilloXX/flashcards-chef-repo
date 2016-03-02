24ab7eb08a7dbd1152e76cd16523beb1435550a6
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-02 16:46:22 +0300

    Add knife vault config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,8 @@ node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+# encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
-Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
+knife[:vault_mode] = "solo"
+# Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
