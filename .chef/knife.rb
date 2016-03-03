9f675a488b62d319b859cc42f919382826c30353
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-03 17:23:36 +0300

    Add flashcards organization config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,24 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
+# cookbook_path    ["cookbooks", "site-cookbooks"]
+# node_path        "nodes"
+# role_path        "roles"
+# environment_path "environments"
+# data_bag_path    "data_bags"
+#encrypted_data_bag_secret "data_bag_key"
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "dolgirev"
+client_key               "#{current_dir}/dolgirev.pem"
+validation_client_name   "flashcards-validator"
+validation_key           "#{current_dir}/flashcards-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/flashcards"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+node_path                ["#{current_dir}/../nodes"]
+role_path                ["#{current_dir}/../roles"]
+environment_path         ["#{current_dir}/../environments"]
+data_bag_path            ["#{current_dir}/../data_bags"]
 # encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
 knife[:vault_mode] = "solo"
-# Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
+Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
