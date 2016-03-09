4f298941d1558ddc940c393c404e69314dec1d6c
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2016-03-09 10:57:10 +0300

    Add config to Chef Zero

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,11 +6,8 @@ environment_path         ["#{current_dir}/../environments"]
 data_bag_path            ["#{current_dir}/../data_bags"]
 log_level                :info
 log_location             STDOUT
-# node_name                "dolgirev"
-# client_key               "#{current_dir}/dolgirev.pem"
-# validation_client_name   "flashcards-validator"
-# validation_key           "#{current_dir}/flashcards-validator.pem"
-# chef_server_url          "https://api.chef.io/organizations/flashcards"
+client_key               "#{current_dir}/dummy.pem"
+chef_server_url          "http://192.168.50.1"
 
 # encrypted_data_bag_secret "data_bag_key"
 
@@ -18,3 +15,11 @@ knife[:editor] = "vim"
 knife[:berkshelf_path] = "cookbooks"
 knife[:vault_mode] = "solo"
 Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
+
+# Config for Hosted Chef
+
+# node_name                "dolgirev"
+# client_key               "#{current_dir}/dolgirev.pem"
+# validation_client_name   "flashcards-validator"
+# validation_key           "#{current_dir}/flashcards-validator.pem"
+# chef_server_url          "https://api.chef.io/organizations/flashcards"
